06e1095fb96963d8a4841beded0dcfa47fd723ce
aiXpertLab <aiXpertLab@gmail.com>
2024-07-24 18:05:47 -0400

    update

--- e101.py
+++
@@ -1,9 +0,0 @@
-import torch
-
-tensor_1 = torch.tensor([0.1, 1, 0.9, 0.7, 0.3])
-tensor_2 = torch.tensor([[0,0.2,0.4,0.6],[1,0.8,0.6,0.4]])
-tensor_3 = torch.tensor([[[0.3,0.5],[1,0],[0.3,0.5],[0,1]]])
-
-print(tensor_1.shape)
-print(tensor_2.shape)
-print(tensor_3.shape)
